ab48aefc8162211314c05a11ee8e7177bb6b87a9
tgcl21 <tgcl21@inf.ufpr.br>
2024-07-31 16:05:41 -0300

    Finds something

--- blocos.c
+++ blocos.c
@@ -1,77 +1,144 @@
+#define NIL -1
 #include <stdio.h>
 #include <stdlib.h>
 #include "blocos.h"
 
-// Protótipos
-void busca_em_profundidade(int vertice, int **matriz_adj, int *visitado, int *visitado_arestas, int n, info_t *info_bloco);
-void imprime_matriz(int **matriz_adj, int n);
+typedef struct {
+    int u, v;
+} Aresta;
 
-void imprime_matriz(int **matriz_adj, int n) {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            printf("%d ", matriz_adj[i][j]);
+typedef struct {
+    int* adj;
+    int tam;
+} AdjList;
+
+typedef struct {
+    AdjList* lista;
+    int V, E;
+} Grafo;
+
+Grafo* cria_grafo(int V, int E);
+void add_aresta(Grafo* grafo, int u, int v);
+void encontra_blocos(Grafo* grafo, int u, int pre[], int low[], Aresta* arestas, int* qtd_arestas, int pais[], int* tempo, int* count, info_t* blocos, int* bloco);
+
+Grafo* cria_grafo(int V, int E) {
+    Grafo* grafo = (Grafo*)malloc(sizeof(Grafo));
+    grafo->V = V;
+    grafo->E = E;
+    grafo->lista = (AdjList*)malloc((size_t)V * sizeof(AdjList));
+    for (int i = 0; i < V; i++) {
+        grafo->lista[i].adj = (int*)malloc((size_t)V * sizeof(int));
+        grafo->lista[i].tam = 0;
+    }
+    return grafo;
+}
+
+void add_aresta(Grafo* grafo, int u, int v) {
+    grafo->lista[u - 1].adj[grafo->lista[u - 1].tam++] = v - 1;
+    grafo->lista[v - 1].adj[grafo->lista[v - 1].tam++] = u - 1;
+}
+
+void encontra_blocos(Grafo* grafo, int u, int pre[], int low[], Aresta* arestas, int* qtd_arestas, int pais[], int* tempo, int* count, info_t* blocos, int* bloco) {
+    pre[u] = low[u] = ++(*tempo);
+    int filho = 0;
+
+    // Para cada vértice adjacente
+    for (int i = 0; i < grafo->lista[u].tam; ++i) {
+        int v = grafo->lista[u].adj[i];
+
+        // Se o vértice não foi visitado
+        if (pre[v] == NIL) {
+            filho++;
+            pais[v] = u;
+            arestas[*qtd_arestas].u = u;
+            arestas[*qtd_arestas].v = v;
+            (*qtd_arestas)++;
+
+            encontra_blocos(grafo, v, pre, low, arestas, qtd_arestas, pais, tempo, count, blocos, bloco);
+
+            low[u] = (low[u] < low[v]) ? low[u] : low[v];
+
+            // Se u é um ponto de articulação
+            if ((pre[u] == 1 && filho > 1) || (pre[u] > 1 && low[v] >= pre[u])) {
+                int vertices_count = 0, arestas_count = 0;
+                while (arestas[*qtd_arestas - 1].u != u || arestas[*qtd_arestas - 1].v != v) {
+                    arestas_count++;
+                    vertices_count++;
+                    (*qtd_arestas)--;
+                }
+                arestas_count++;
+                vertices_count++;
+                (*qtd_arestas)--;
+                blocos[*bloco].vertices = (unsigned int)vertices_count;
+                blocos[*bloco].arestas = (unsigned int)arestas_count;
+                (*bloco)++;
+                (*count)++;
+            }
+        } else if (v != pais[u]) { // Se v não é o pai de u
+            low[u] = (low[u] < pre[v]) ? low[u] : pre[v];
+            if (pre[v] < pre[u]) { 
+                arestas[*qtd_arestas].u = u;
+                arestas[*qtd_arestas].v = v;
+                (*qtd_arestas)++;
+            }
         }
-        printf("\n");
     }
 }
 
-void busca_em_profundidade(int vertice, int **matriz_adj, int *visitado, int *visitado_arestas, int n, info_t *info_bloco) {
-    visitado[vertice] = 1; // Marca vértice como visitado
-    info_bloco->vertices++; // Incrementa número de vértices do bloco
-    for (int i = 0; i < n; i++) {
-        if (matriz_adj[vertice][i]) {
-            int aresta_id = vertice * n + i;
-            if (!visitado_arestas[aresta_id]) {
-                visitado_arestas[aresta_id] = 1;
-                visitado_arestas[i * n + vertice] = 1;
-                info_bloco->arestas++;
-            }
-            if (!visitado[i]) {
-                busca_em_profundidade(i, matriz_adj, visitado, visitado_arestas, n, info_bloco);
-            }
-        }
-    }
-}
+info_t* info_blocos(unsigned int* num_blocos) {
+    int V;
+    scanf("%d", &V);
 
-info_t *info_blocos(unsigned int *num_blocos) {
-    int n;
-    scanf("%d", &n);
-    // Alocar matriz de adjacência
-    int **matriz_adj = malloc((size_t)n * sizeof(int *));
-    for (int i = 0; i < n; i++) {
-        matriz_adj[i] = calloc((size_t)n, sizeof(int));
-    }
+    Grafo* grafo = cria_grafo(V, 0);
 
-    // Lê arestas
     int u, v;
     while (scanf("%d %d", &u, &v) != EOF) {
-        matriz_adj[u - 1][v - 1] = 1;
-        matriz_adj[v - 1][u - 1] = 1;
+        add_aresta(grafo, u, v);
+        grafo->E++;
     }
 
-    int *visitado = calloc((size_t)n, sizeof(int)); 
-    int *visitado_arestas = calloc((size_t)(n * n), sizeof(int)); // Marcar arestas visitadas (evita erros de contagem)
-    info_t *blocos = malloc((size_t)n * sizeof(info_t));
-    *num_blocos = 0;
+    int* pre = (int*)malloc((size_t)V * sizeof(int)); // Pre para cada vértice
+    int* low = (int*)malloc((size_t)V * sizeof(int)); // Low point para cada vértice
+    int* pais = (int*)malloc((size_t)V * sizeof(int)); // Pai de cada vértice
+    Aresta* arestas = (Aresta*)malloc((size_t)grafo->E * sizeof(Aresta));  // Para armazenar as arestas
+    int qtd_arestas = 0;
 
-    // Encontrar blocos
-    for (int i = 0; i < n; i++) {
-        // Se o vértice não foi visitado, então é um novo bloco
-        if (!visitado[i]) {
-            // Inicializa bloco 
-            blocos[*num_blocos].vertices = 0;
-            blocos[*num_blocos].arestas = 0;
-            busca_em_profundidade(i, matriz_adj, visitado, visitado_arestas, n, &blocos[*num_blocos]);
-            (*num_blocos)++;
+    // Seta todos os valores iniciais como -1
+    for (int i = 0; i < V; i++) {
+        pre[i] = NIL;
+        low[i] = NIL;
+        pais[i] = NIL;
+    }
+
+    *num_blocos = 0;
+    info_t* blocos = (info_t*)malloc((size_t)grafo->E * sizeof(info_t));
+    int tempo = 0;
+    int count = 0;
+    int bloco = 0;
+
+    // Encontra os blocos
+    for (int i = 0; i < V; i++) {
+        if (pre[i] == NIL) {
+            encontra_blocos(grafo, i, pre, low, arestas, &qtd_arestas, pais, &tempo, &count, blocos, &bloco);
+
+            if (qtd_arestas > 0) {
+                int vertices_count = 0, arestas_count = 0;
+            
+                // Conta os vértices e arestas do último bloco
+                while (qtd_arestas > 0) {
+                    arestas_count++;
+                    vertices_count++;
+                    qtd_arestas--;
+                }
+
+                blocos[bloco].vertices = (unsigned int)vertices_count;
+                blocos[bloco].arestas = (unsigned int)arestas_count;
+                bloco++;
+                count++;
+            }
         }
     }
 
-    free(visitado);
-    free(visitado_arestas);
-    for (int i = 0; i < n; i++) {
-        free(matriz_adj[i]);
-    }
-    free(matriz_adj);
-
+    *num_blocos = (unsigned int)count;
     return blocos;
 }
